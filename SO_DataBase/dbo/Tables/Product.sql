3339cf5105498942ad75fda64882357b3fc40524
Marco Preciado <mac46005@gmail.com>
2021-01-27 22:55:43 -0600

    Continue to work on control groups for add new product

--- SO_DataBase/dbo/Tables/Product.sql
+++ SO_DataBase/dbo/Tables/Product.sql
@@ -5,7 +5,7 @@
     [GeneralDetails_Id] INT NOT NULL, 
     [RetailPrice] MONEY NOT NULL, 
     [Tax_Id] INT NOT NULL, 
-    [QuantityStock] INT NOT NULL, 
+    [QuantityStock] INT NOT NULL DEFAULT 0, 
     [IsAvailable] BIT NOT NULL, 
     [CreateDate] DATETIME2 NOT NULL, 
     [LastModified] DATETIME2 NOT NULL, 
