794537d415c533f8000b449fb4f052c8a25db8de
Marco Preciado <mac46005@gmail.com>
2021-01-28 16:07:36 -0600

    Continue working on ADMIN

---
+++ StoreOutline2/Areas/Admin/Controllers/ProductInfoManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreOutline2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class ProductInfoManager : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
