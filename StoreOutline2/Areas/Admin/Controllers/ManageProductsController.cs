ef558f5ef34e053b3c427cb79875fbff027c1cfc
Marco Preciado <mac46005@gmail.com>
2021-01-27 20:03:04 -0600

    Currently Modifying Admin

---
+++ StoreOutline2/Areas/Admin/Controllers/ManageProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreOutline2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]")]
+    public class ManageProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
