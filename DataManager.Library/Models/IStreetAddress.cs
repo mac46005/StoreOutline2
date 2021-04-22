6c5d8e86bdef28d898c228e1f1376515bf9008ec
Marco Preciado <mac46005@gmail.com>
2021-04-21 22:15:08 -0500

    Created Supplier Model

---
+++ DataManager.Library/Models/IStreetAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataManager.Library.Models
+{
+    interface IStreetAddress
+    {
+        public string Address { get; set; }
+        public string StateProvince { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ DataManager.Library/Models/SupplierModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataManager.Library.Models
+{
+    public class SupplierModel
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public string Address { get; set; }
+        public string StateProvince { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
